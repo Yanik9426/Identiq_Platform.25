fa488eab9dae3d5ca7cae3e5fd64d4ef45dfd2c3
Your Name <your.email@example.com>
2025-04-21 15:01:45 +0200

    Use absolute path for webAPIs.js import

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 // src/setupTests.ts
 // Import the web API polyfills first
-require('./mocks/webAPIs.js');
+require(`${process.cwd()}/src/mocks/webAPIs.js`);
 
 // Now safe to import testing libraries
 import '@testing-library/jest-dom';
